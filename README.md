be93a162fbe7e4d744228240748f30b6ed0bf70e
mutoe <mutoe@foxmail.com>
2020-06-24 22:20:34 +0800

    chore: initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mutoe's Personal Hammerspoon Config
+
+- WinWin (interactive resize app window)
+- CountDown (display a deadline at the bottom of the screen)
+- CursorJump (jump the mouse cursor to next screen)
+
+# Reference 
+
+- [Spoons - Hammerspoon](https://github.com/Hammerspoon/Spoons/tree/master/Source)
+- [awesome-hammerspoon](https://github.com/ashfinal/awesome-hammerspoon)
---
+++ private/config.lua
@@ -0,0 +1,62 @@
+-- Specify Spoons which will be loaded
+hspoon_list = {
+    -- "AClock",
+    -- "BingDaily",
+    -- "Calendar",
+    -- "CircleClock",
+    -- "ClipShow",
+    "CountDown",
+    -- "CursorJump",
+    -- "FnMate",
+    -- "HCalendar",
+    -- "HSaria2",
+    -- "HSearch",
+    "KSheet",
+    -- "SpeedMenu",
+    -- "TimeFlow",
+    -- "UnsplashZ",
+    "WinWin",
+}
+
+-- Modal supervisor keybinding, which can be used to temporarily disable ALL modal environments.
+hsupervisor_keys = {{"cmd", "shift", "ctrl"}, "Q"}
+
+-- Reload Hammerspoon configuration
+hsreload_keys = {{"cmd", "shift", "ctrl"}, "R"}
+
+-- Toggle help panel of this configuration.
+hshelp_keys = {{"alt", "shift"}, "/"}
+
+----------------------------------------------------------------------------------------------------
+-- Those keybindings below could be disabled by setting to {"", ""} or {{}, ""}
+
+
+-- clipshowM environment keybinding: System clipboard reader
+-- hsclipsM_keys = {"alt", "C"}
+
+-- Toggle the display of aria2 frontend
+-- hsaria2_keys = {"alt", "D"}
+
+-- Launch Hammerspoon Search
+-- hsearch_keys = {"alt", "G"}
+
+-- countdownM environment keybinding: Visual countdown
+hscountdM_keys = {"alt", "I"}
+
+-- Lock computer's screen
+-- hslock_keys = {"alt", "L"}
+
+-- resizeM environment keybinding: Windows manipulation
+hsresizeM_keys = {"alt", "R"}
+
+-- cheatsheetM environment keybinding: Cheatsheet copycat
+hscheats_keys = {"alt", "S"}
+
+-- Show digital clock above all windows
+hsaclock_keys = {"alt", "T"}
+
+-- Toggle Hammerspoon console
+hsconsole_keys = {"alt", "Z"}
+
+-- Jump cursor to next screen
+hscursor_jump_keys = {"alt", "A"}
