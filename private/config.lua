5797a464988c6ed481b7471b0a8d4b40c2f5e812
mutoe <mutoe@foxmail.com>
2020-10-28 12:07:44 +0800

    remove useless plugins

--- private/config.lua
+++ private/config.lua
@@ -1,21 +1,8 @@
 -- Specify Spoons which will be loaded
 hspoon_list = {
-    -- "AClock",
-    -- "BingDaily",
-    -- "Calendar",
-    -- "CircleClock",
-    -- "ClipShow",
-    "CountDown",
-    -- "CursorJump",
-    -- "FnMate",
-    -- "HCalendar",
-    -- "HSaria2",
-    -- "HSearch",
-    "KSheet",
-    -- "SpeedMenu",
-    -- "TimeFlow",
-    -- "UnsplashZ",
-    "WinWin",
+  "CountDown",
+  "KSheet",
+  "WinWin",
 }
 
 -- Modal supervisor keybinding, which can be used to temporarily disable ALL modal environments.
@@ -25,34 +12,19 @@ hspoon_list = {
 -- hsreload_keys = {{"cmd", "shift", "ctrl"}, "R"}
 
 -- Toggle help panel of this configuration.
-hshelp_keys = {{"cmd", "ctrl", "shift"}, "/"}
+hshelp_keys = { { "cmd", "ctrl", "shift" }, "/" }
 
 ----------------------------------------------------------------------------------------------------
 -- Those keybindings below could be disabled by setting to {"", ""} or {{}, ""}
 
--- clipshowM environment keybinding: System clipboard reader
--- hsclipsM_keys = {"alt", "C"}
-
--- Toggle the display of aria2 frontend
--- hsaria2_keys = {"alt", "D"}
-
--- Launch Hammerspoon Search
--- hsearch_keys = {"alt", "G"}
-
 -- countdownM environment keybinding: Visual countdown
-hscountdM_keys = {{"cmd", "ctrl"}, "D"}
-
--- Lock computer's screen
--- hslock_keys = {"alt", "L"}
+hscountdM_keys = { { "cmd", "ctrl" }, "D" }
 
 -- resizeM environment keybinding: Windows manipulation
-hsresizeM_keys = {{"cmd", "ctrl"}, "L"}
+hsresizeM_keys = { { "cmd", "ctrl" }, "L" }
 
 -- cheatsheetM environment keybinding: Cheatsheet copycat
-hscheats_keys = {{"cmd", "ctrl"}, "X"}
+hscheats_keys = { { "cmd", "ctrl" }, "X" }
 
 -- Toggle Hammerspoon console
 -- hsconsole_keys = {"alt", "Z"}
-
--- Jump cursor to next screen
-hscursor_jump_keys = {{"ctrl", "cmd"}, "J"}
